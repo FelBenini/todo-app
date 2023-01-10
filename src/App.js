e92972e9c02faa65c5bc2d8f96bc1be8f9eaac02
FelBenini <112193194+FelBenini@users.noreply.github.com>
2023-01-09 21:50:39 -0300

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   src/App.js 	modified:   src/components/todo-item.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,53 @@ import './App.css';
 import { Cookies } from 'react-cookie';
 import { useState } from 'react';
 import TodoItem from './components/todo-item';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 const cookies = new Cookies()
 
 function App() {
+  const [alignment, setAlignment] = useState('personal')
   const [arrayOfObjects, setArrayOfObjects] = useState(cookies.get('arrayOfObjects') || []);
   const [name, setName] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
-    const newObject = { id: Date.now(), name, situation: "incomplete", type: "professional" };
+    const newObject = { id: Date.now(), name, situation: "incomplete", type: alignment };
     setArrayOfObjects([...arrayOfObjects, newObject]);
     cookies.set('arrayOfObjects', [...arrayOfObjects, newObject], { path: '/' });
     setName('')
   }
+
+    const handleAlignment = (event, newAlignment) => {
+    if (newAlignment !== null) {
+      setAlignment(event.target.value);
+    }
+  };
+
   return (
     <div>
       {arrayOfObjects.map((object) => (
-        <TodoItem type={object.type} name={object.name}/>
+        <TodoItem id={object.id} type={object.type} name={object.name}/>
       ))}
       <form onSubmit={handleSubmit}>
+        <ToggleButtonGroup
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="text alignment"
+      >
+        <ToggleButton value="personal" aria-label="left aligned" sx={{ borderRadius: '50px', width: '300px'}}>
+          Personal
+        </ToggleButton>
+        <ToggleButton value="professional" aria-label="centered" sx={{ borderRadius: '50px', width: '300px'}}>
+          Professional
+        </ToggleButton>
+      </ToggleButtonGroup>
+
         <label>
           Name:
+          
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
         <input type="submit" value="Submit" />
--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -4,13 +4,13 @@ function TodoItem(props) {
     if (props.type === "professional") {
         return (
             <div className='todo-item professional'>
-                {props.name}
+                {props.name} {props.type}
             </div>
         )
     } else if (props.type === "personal") {
         return (
             <div className='todo-item personal'>
-                {props.name}
+                {props.name} {props.type}
             </div>
         )
     }
