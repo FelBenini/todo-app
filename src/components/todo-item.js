0d5acedfc2faa8fa7b209e825e68979bf8362dd7
FelBenini <112193194+FelBenini@users.noreply.github.com>
2023-01-10 22:25:41 -0300

    modified:   src/App.css 	modified:   src/components/todo-item.js

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -4,14 +4,14 @@ function TodoItem(props) {
     if (props.type === "professional") {
         return (
             <div className='todo-item professional'>
-                <h3>{props.type}</h3>
+                <h5>{props.type}</h5>
                 <p>{props.name}</p>
             </div>
         )
     } else if (props.type === "personal") {
         return (
             <div className='todo-item personal'>
-                <h3>{props.type}</h3>
+                <h5>{props.type}</h5>
                 <p>{props.name}</p>
             </div>
         )
