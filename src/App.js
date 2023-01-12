dfbcbf7afa847dff00bc3e4518ffb8208d1a75ec
FelBenini <112193194+FelBenini@users.noreply.github.com>
2023-01-12 18:00:56 -0300

    Changed the cookies logic to work with context

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
 import './App.css';
-import { Cookies } from 'react-cookie';
-import { useState } from 'react';
 import TodoItem from './components/todo-item';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Header from './components/header';
 import { CookieState } from './cookiesContext';
-
-const cookies = new Cookies()
+import Form from './components/form';
 
 function App() {
-  const [type, setType] = useState('personal')
-  const {arrayOfTodos, setArrayOfTodos} = CookieState()
-  const [name, setName] = useState('');
-  const [borderColor, setBorderColor] = useState()
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (name !== "") {
-      const newObject = { id: Date.now(), name, situation: "incomplete", type: type };
-      setArrayOfTodos([newObject, ...arrayOfTodos ]);
-      cookies.set('arrayOfTodos', [newObject, ...arrayOfTodos], { path: '/' });
-      setName('')
-    }
-  }
-
-  const handleTypeChange = (event, newAlignment) => {
-    if (newAlignment !== null) {
-      setType(event.target.value);
-    }
-    if (type === "professional") {
-      setBorderColor('#09CAAD')
-    } else if (type === "personal") {
-      setBorderColor('#D58042')
-    }
-  };
+  const {arrayOfTodos} = CookieState()
 
   return (
     <main>
       <Header/>
       <img src="/img/logo.svg" alt="todo app logo" id="todo-app-logo"/>
-      <form id="form-todo" onSubmit={handleSubmit}>
-        <ToggleButtonGroup value={type} exclusive onChange={handleTypeChange} aria-label="text alignment">
-          <ToggleButton value="personal" aria-label="left aligned" sx={{ borderRadius: '4px', width: '125px', height: '40px', textTransform: 'none', fontFamily: "'Raleway', sans-serif" }} color="primary">
-            Personal
-          </ToggleButton>
-          <ToggleButton value="professional" aria-label="centered" sx={{ borderRadius: '4px', width: '125px', height: '40px', textTransform: 'none', fontFamily: "'Raleway', sans-serif" }} color="secondary">
-            Professional
-          </ToggleButton>
-        </ToggleButtonGroup>
-
-        <textarea placeholder={`Type your ${type} task here...`} style={{ border: `2px solid ${borderColor}80` }} value={name} onChange={(e) => setName(e.target.value)} />
-        <button style={{backgroundColor: borderColor}} type="submit" value="Submit">Post Todo<img src="/img/bx-plus.svg" alt="plus icon"/></button>
-      </form>
+      <Form/>
       <section id="todo-display">
       {arrayOfTodos.map((object) => (
         <TodoItem id={object.id} type={object.type} name={object.name} />
---
+++ src/components/form.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { CookieState } from '../cookiesContext';
+import { useState } from 'react';
+import { Cookies } from 'react-cookie';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+const cookies = new Cookies()
+
+function Form() {
+    const { arrayOfTodos, setArrayOfTodos } = CookieState()
+    const [name, setName] = useState('');
+    const [borderColor, setBorderColor] = useState()
+    const [type, setType] = useState('personal')
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (name !== "") {
+            const newObject = { id: Date.now(), name, situation: "incomplete", type: type };
+            setArrayOfTodos([newObject, ...arrayOfTodos]);
+            cookies.set('arrayOfTodos', [newObject, ...arrayOfTodos], { path: '/' });
+            setName('')
+        }
+    }
+
+    const handleTypeChange = (event, newAlignment) => {
+        if (newAlignment !== null) {
+            setType(event.target.value);
+        }
+        if (type === "professional") {
+            setBorderColor('#09CAAD')
+        } else if (type === "personal") {
+            setBorderColor('#D58042')
+        }
+    };
+    return (
+        <form id="form-todo" onSubmit={handleSubmit}>
+            <ToggleButtonGroup value={type} exclusive onChange={handleTypeChange} aria-label="text alignment">
+                <ToggleButton value="personal" aria-label="left aligned" sx={{ borderRadius: '4px', width: '125px', height: '40px', textTransform: 'none', fontFamily: "'Raleway', sans-serif" }} color="primary">
+                    Personal
+                </ToggleButton>
+                <ToggleButton value="professional" aria-label="centered" sx={{ borderRadius: '4px', width: '125px', height: '40px', textTransform: 'none', fontFamily: "'Raleway', sans-serif" }} color="secondary">
+                    Professional
+                </ToggleButton>
+            </ToggleButtonGroup>
+
+            <textarea placeholder={`Type your ${type} task here...`} style={{ border: `2px solid ${borderColor}80` }} value={name} onChange={(e) => setName(e.target.value)} />
+            <button style={{ backgroundColor: borderColor }} type="submit" value="Submit">Post Todo<img src="/img/bx-plus.svg" alt="plus icon" /></button>
+        </form>
+    )
+}
+
+export default Form
